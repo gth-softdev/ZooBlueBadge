9e87c370a179ed663876dc8414a1dbf896b95425
KateLockhart <katelockhart707@gmail.com>
2020-09-03 16:08:35 -0400

    Created Zoo class and properties

--- ZooBlue.Data/Zoo.cs
+++ ZooBlue.Data/Zoo.cs
@@ -9,5 +9,15 @@ namespace ZooBlue.Data
     public class Zoo
     {
 
+        public int ZooId { get; set; }
+        public string ZooName { get; set; }
+        public string Location { get; set; }
+        public double ZooSize { get; set; }
+        public bool AZAAccredited { get; set; }
+        public double Admission { get; set; }
+        public Review AverageRating { get; }
+        public List<Attraction> Attractions { get; }
+        public List<Review> Reviews { get; }
+
     }
 }
