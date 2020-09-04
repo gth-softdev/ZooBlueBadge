13121f71aa8288cd88249e2ce140b33e6ac70f90
KateLockhart <katelockhart707@gmail.com>
2020-09-04 09:48:50 -0400

    Zoo.cs updates

--- ZooBlue.Data/Zoo.cs
+++ ZooBlue.Data/Zoo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,15 @@ namespace ZooBlue.Data
 {
     public class Zoo
     {
-
+        [Key]
         public int ZooId { get; set; }
+        [Required]
         public string ZooName { get; set; }
+        [Required]
         public string Location { get; set; }
         public double ZooSize { get; set; }
         public bool AZAAccredited { get; set; }
+        [Required]
         public double Admission { get; set; }
         public Review AverageRating { get; }
         public List<Attraction> Attractions { get; }
