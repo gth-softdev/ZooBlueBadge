efaed3e4a84dbff10d88256590cef0fe61bd439a
KateLockhart <katelockhart707@gmail.com>
2020-09-08 10:49:14 -0400

    Zoo Models completed

---
+++ ZooBlue.Models/ZooModels/ZooDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBlue.Models.ZooModels
+{
+    public class ZooDetail
+    {
+        public int ZooId { get; set; }
+        public string ZooName { get; set; }
+        public string Location { get; set; }
+        public double ZooSize { get; set; }
+        public bool AZAAccredited { get; set; }
+        public double Admission { get; set; }
+    }
+}
